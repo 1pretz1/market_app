15400b232b113cad01e2e2656189a40bc75bd2e7
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2017-09-25 10:25:56 +0100

    Login, product adding and listing, user sessions
    basic functionality of market website, including uploading photo's,
    index and show of products. User sessions and specific products to each
    user.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+
+  default :from => "application_name@domain.com"
+
+  def registration_confirmation(user)
+    @user = user
+    mail(:to => "#{user.name} <#{user.email}, :subject => "Registration Confirmation for Awesome App)
+  end
+
+
+end
